b5ba9b319a4fcc60c438ef5daf6030b01c3a58ed
wendyhotdog <wendyhotdog101@gmail.com>
2023-01-19 13:57:57 +0800

    show page fully functional

--- resources/js/Pages/Facility/Show.vue
+++ resources/js/Pages/Facility/Show.vue
@@ -1,62 +1,70 @@
 <template>
     <AuthenticatedLayout>
-        <section class="text-gray-600 body-font">
-            <div class="container px-5 py-24 mx-auto flex flex-col">
-                <div class="lg:w-5/6 mx-auto border-[2px] rounded">
-                    <div class="rounded-lg h-64 overflow-hidden">
-                        <img
-                            alt="content"
-                            class="object-cover object-center h-full w-full"
-                            :src="facility.image"
-                        />
-                    </div>
-                    <div class="flex flex-col sm:flex-row mt-10">
-                        <div class="sm:w-1/3 text-center sm:pr-8 sm:py-8">
-                            <div
-                                class="w-20 h-20 rounded-full inline-flex items-center justify-center bg-gray-200 text-gray-400"
-                            ></div>
-                            <div
-                                class="flex flex-col items-center text-center justify-center"
-                            >
-                                <h2
-                                    class="font-medium title-font mt-4 text-gray-900 text-lg dark:text-white"
-                                >
-                                    {{ facility.name }}
-                                </h2>
-                                <div
-                                    class="w-12 h-1 bg-indigo-500 rounded mt-2 mb-4"
-                                ></div>
-                                <p class="text-base dark:text-white">
-                                    RM{{ facility.rate_per_hour }}/hour
-                                </p>
-                            </div>
-                        </div>
+        <div
+            class="px-3 md:lg:xl:px-40 bg-cover border-t border-b py-20 bg-opacity-10"
+            style="background-image: url('public/images/mainbg.jpg')"
+        >
+            <article class="rounded-xl bg-white p-3 shadow-lg mb-4">
+                <a href="#">
+                    <div
+                        class="relative flex items-end overflow-hidden rounded-xl max-h-[20rem]"
+                    >
+                        <img :src="facility.image" />
                         <div
-                            class="sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-gray-200 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left"
+                            class="flex items-center space-x-1.5 rounded-lg bg-blue-500 px-4 py-1.5 text-white duration-100 hover:bg-blue-600"
                         >
-                            <p
-                                class="leading-relaxed text-lg mb-4 dark:text-gray-200 mb-24"
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke-width="1.5"
+                                stroke="currentColor"
+                                class="h-4 w-4"
                             >
-                                {{ facility.description }}
-                            </p>
-                            <!-- Align Right -->
-                            <button class="bg-green-500 rounded p-2 text-white">
-                                Book this Facility
-                            </button>
+                                <path
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+                                />
+                            </svg>
                         </div>
                     </div>
-                </div>
-            </div>
-        </section>
+
+                    <div class="mt-1 p-2">
+                        <h2 class="text-slate-700">IOI City Mall</h2>
+                        <p class="mt-1 text-sm text-slate-400">
+                            IOI City, Putrajaya
+                        </p>
+
+                        <div class="mt-3 flex items-end justify-between">
+                            <p>
+                                <span class="text-lg font-bold text-teal-600"
+                                    >Badminton Court</span
+                                >
+                            </p>
+                        </div>
+                    </div>
+                </a>
+            </article>
+
+            <div
+                class="grid grid-cols-3 md:lg:xl:grid-cols-3 group bg-white shadow-xl shadow-neutral-100 border shadow-xl shadow-teal-300"
+            ></div>
+
+            <!-- Reservation create goes here -->
+            <ReservationCreate :facility="facility" />
+        </div>
     </AuthenticatedLayout>
 </template>
 <script>
 import { defineComponent } from "vue";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import ReservationCreate from "@/Pages/Reservation/Create.vue";
 export default defineComponent({
     name: "FacilityShow",
     components: {
         AuthenticatedLayout,
+        ReservationCreate,
     },
     props: {
         facility: Object,
---
+++ resources/js/Pages/Reservation/Create.vue
@@ -0,0 +1,125 @@
+<template>
+    <div
+        class="w-full bg-slate-50 shadow-xl shadow-teal-300 py-10 px-20 items-center"
+    >
+        <h1 class="text-2xl font-medium mb-5">Choose your slot</h1>
+
+        <div class="-mx-3 flex flex-wrap">
+            <div class="w-full px-3 sm:w-1/2">
+                <div class="mb-5">
+                    <label
+                        for="date"
+                        class="mb-3 block text-base font-medium text-teal-600"
+                    >
+                        Date
+                    </label>
+                    <input
+                        type="date"
+                        name="date"
+                        id="date"
+                        v-model="this.date"
+                        class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-teal-600 focus:shadow-md"
+                        min="{{ new Date().toISOString().substr(0, 10) }}"
+                    />
+                </div>
+            </div>
+            <div class="w-full px-3 sm:w-1/2">
+                <div class="mb-5">
+                    <label
+                        for="time"
+                        class="mb-3 block text-base font-medium text-teal-600"
+                    >
+                        Time
+                    </label>
+                    <input
+                        type="time"
+                        name="time"
+                        id="time"
+                        v-model="this.time"
+                        class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:teal-600 focus:shadow-md"
+                    />
+                </div>
+            </div>
+        </div>
+
+        <div class="mb-5">
+            <label
+                for="duration"
+                class="mb-3 block text-base font-medium text-teal-600"
+            >
+                Select your duration (hours)
+            </label>
+            <input
+                type="number"
+                name="duration"
+                id="duration"
+                placeholder="1"
+                min="1"
+                max="4"
+                v-model="this.duration"
+                class="w-full appearance-none rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-teal-600 focus:shadow-md"
+            />
+        </div>
+
+        <div class="relative w-full lg:mt-0 mt-6" role="list">
+            <div
+                role="listitem"
+                class="bg-gray-100 cursor-pointer rounded-lg mt-3 flex relative shadow-lg hover:shadow-xl"
+            >
+                <div
+                    class="w-2.5 h-auto bg-teal-600 rounded-tl-md rounded-bl-md"
+                ></div>
+                <div class="w-full px-8 py-2">
+                    <div class="md:flex items-center justify-between">
+                        <h2
+                            class="text-xl font-semibold leading-6 text-gray-800"
+                        >
+                            9 P.M
+                        </h2>
+                        <p
+                            class="text-xl md:mt-0 mt-4 leading-6 text-lg font-bold text-teal-600"
+                        >
+                            <span class="text-base">RM {{ total }}</span>
+                        </p>
+                    </div>
+                    <p class="md:w-80 text-base leading-6 text-gray-600">
+                        {{ this.time }} - {{ endTime }}
+                    </p>
+                    <p class="md:w-80 text-base leading-6 text-gray-600">
+                        {{ this.duration }} hours
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import { defineComponent } from "vue";
+export default defineComponent({
+    props: {
+        facility: {
+            type: Object,
+            required: true,
+        },
+    },
+    data() {
+        return {
+            date: new Date().toISOString().substr(0, 10),
+            time: new Date().toISOString().substr(11, 5),
+            duration: null,
+        };
+    },
+    computed: {
+        total() {
+            // Limit it to 2 decimal places
+            return (this.facility.rate_per_hour * this.duration).toFixed(2);
+        },
+        endTime() {
+            // parse time and add on the amount of duration in hours into the time
+            const time = new Date("1970-01-01T" + this.time + "Z").getTime();
+            const endTime = new Date(time + this.duration * 60 * 60 * 1000);
+            return endTime.toISOString().substr(11, 5);
+        },
+    },
+});
+</script>
