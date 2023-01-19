01aaa265152b313c225a1aff414480ae2547db5b
wendyhotdog <wendyhotdog101@gmail.com>
2023-01-19 23:15:38 +0800

    index

--- resources/js/Pages/Admin/Facility/Index.vue
+++ resources/js/Pages/Admin/Facility/Index.vue
@@ -12,6 +12,13 @@
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+
+                <!--modal start-->
+
+                
+
+                <!--modal end-->
+
                 <div
                     class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg"
                 >
